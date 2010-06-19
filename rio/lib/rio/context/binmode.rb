284924bc5357b81e29340a334496fa0f47270f4f
rio4ruby <rio4ruby@rio4ruby.com>
2010-06-19 07:37:23 +0000

    change how binmode is handled
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rio/trunk@413 15e1cb93-1811-0410-b9d1-a500f5877eb0

---
+++ rio/lib/rio/context/binmode.rb
@@ -0,0 +1,87 @@
+#--
+# ===========================================================================
+# Copyright (c) 2005,2006,2007,2008,2009,2010 Christopher Kleckner
+# All rights reserved
+#
+# This file is part of the Rio library for ruby.
+#
+# Rio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rio; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# =========================================================================== 
+#++
+#
+#
+# ==== Rio - Ruby I/O Facilitator
+#
+# Rio is a facade for most of the standard ruby classes that deal with
+# I/O; providing a simple, intuitive, succinct interface to the
+# functionality provided by IO, File, Dir, Pathname, FileUtils,
+# Tempfile, StringIO, OpenURI and others. Rio also provides an
+# application level interface which allows many common I/O idioms to be
+# expressed succinctly.
+#
+# ===== Suggested Reading
+#
+# * RIO::Doc::SYNOPSIS
+# * RIO::Doc::INTRO
+# * RIO::Doc::HOWTO
+# * RIO::Doc::EXAMPLES
+# * RIO::Rio
+#
+# Project::       http://rubyforge.org/projects/rio/
+# Documentation:: http://rio4ruby.com/
+# Bugs::          http://rubyforge.org/tracker/?group_id=821
+# Blog::          http://rio4ruby.blogspot.com/
+# Email::         rio4ruby@rubyforge.org
+#
+
+
+#require 'rio/context/cxx.rb'
+#require 'rio/iomode'
+
+require 'rio/context/stream'   
+require 'rio/context/dir'   
+require 'rio/context/skip'   
+#require 'rio/filter'   
+require 'rio/context/autoclose'   
+require 'rio/context/gzip'   
+require 'rio/context/copying'   
+require 'rio/context/encoding'   
+
+
+module RIO
+
+  module Cx
+    module Methods
+      def binmode(&block) 
+        ioh.binmode unless ioh.nil?
+        cxx('binmode',&block) 
+      end
+      def binmode?() 
+        setting = cxx?('binmode')
+        if ioh
+          actual = ioh.binmode?
+          cxx_('binmode',actual) unless actual == setting
+        end
+        cxx?('binmode')
+      end 
+      def binmode_(arg=true)  
+        cxx_('binmode',arg) 
+      end
+      protected :binmode_
+    end
+  end
+
+
+end
